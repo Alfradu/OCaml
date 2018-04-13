400b944caa871dc5ba4ad8ff69cbadfbfb9a9ed5
sumaris <alfred.radu@gmail.com>
2018-04-13 14:50:24 +0200

    asdasd

--- OCaml Lab1.ml
+++ OCaml Lab1.ml
@@ -1,82 +1,83 @@
-(*Lab 1 RelAlg*)
-module type TABLE =
-  sig
-    type dtable
-    type dcolumn
-    type attribute
-    val newDtable : dtable
-    val newDcolumns : dcolumn
-    val newAttributes : attribute
-    val projection : attribute list * dtable -> dtable
-    val rename : attribute * attribute * dtable -> dtable
-    val replace : attribute * attribute * dtable -> dcolumn list
-    val restriction : attribute * string * dtable -> dtable
-    val restrictColumns : string list * dcolumn list -> dcolumn list
-    val restrictColumn : string list * dcolumn -> dcolumn
-    val generateBoolList : string * dcolumn list -> bool list
-    val getColumn : dtable * attribute -> dcolumn
-  end;;
-module TinyRelationalAlgebra : TABLE =
-  struct
-    exception DoesNotExist
-    type dtable = string * dcolumn list
-    type dcolumn = string * attribute list
-    type attribute = string
-    let fst (a,b) = a
-    let snd (a,b) = b
-    (*generate basic table*)
-    let newAttributes = []
-    let newDcolumns = []
-    let newDtable = []
+exception DoesNotExist
+type attribute = string
+type dcolumn = string * attribute list
+type dtable = string * dcolumn list
+type dbase = dtable list
+type relalg = Project of (string list * string)
+            | Rename of (string * string * string)
+            | Restrict of (string * string * string)
 
-(* PROJECTION *)
-    let rec projection (attributes, dtable) =
-      match attributes with
-        []     -> ""
-      | [h::t] -> getColumn(dtable, h); projection(t, dtable)
+(* General *)
+let table = ("Student", [
+   ("Id", ["2";"4";"7";"9"]);
+   ("Name", ["Jim";"Linnea";"Steve";"Hannah"]);
+   ("Gender",["Male";"Female";"Male";"Female"]);
+   ("Course",["Geography";"Economics";"Informatics";"Geography"])
+])
 
-(* REPLACE *)
-    let rename (attOld, attNew, dtable) =
-      match dtable(name, dcolumn) with
-        (name, dcolumn) -> (name, replace(attOld, attNew, dcolumn))
-    let rec replace (attOld, attNew, dcolumn) =
-      match dcolumn with
-        []     -> raise DoesNotExist
-      | [h::t] -> if fst(h) = attOld
-                  then fst(h) = attNew; h::(replace(attOld, attNew, (name, t)))
-                  else h::(replace(attOld, attNew, (name, t)))
+let dbase = ref [table]
 
-(* RESTRICTION *)
-    let restriction (attribute, val, dtable) =
-      let boolList = generateBoolList(val, getColumn(dtable, attribute)) in
-        match dtable(name, columns) with
-          (_, columns) -> (name, restrictColumns(boolList, columns))
-    let rec restrictColumns (boolList, columns) =
-      match columns with
-        []     -> []
-      | [h::t] -> restrictColumn(boolList, h)::(restrictColumns(boolList, t))
-    let rec restrictColumn (boolList, column) =
-      match (boolList, column) with
-        ([], _)             -> []
-      | ([h1::t1],[h2::t2]) -> if h1 = true
-                               then h2::(restrictColumn(t1, t2))
-                               else restrictColumn(t1, t2)
-                               
-(* GENERAL FUNCTIONS *)
-    (* GenerateBoolList *)
-    (* generates a list of booleans.*)
-    let rec generateBoolList (val, column) =
-      match column(attribute, values) with
-        (_ ,[])     -> []
-      | (_,[h::t])  -> if val = h
-                       then ["true"]::(generateBoolList(val ,t))
-                       else ["false"]::(generateBoolList(val ,t))
-    (* getColumn *)
-    (* returns a column of a given attribute.*)
-    let rec getColumn (dtable, attribute) =
-      match dtable(name, columns) with
-        (name, [])     -> raise DoesNotExist
-      | (name, [h::t]) -> if fst(h) = attribute
-                          then snd(h)
-                          else getColumn((name, t), attribute)
-end;;
+let rec getColumn (dtable, attribute) =
+  match dtable with
+    (name, [])       -> raise DoesNotExist
+  | (name, (a,b)::t) -> if a = attribute
+                        then (a,b)
+                        else getColumn((name, t), attribute)
+
+let rec generateBoolList (a, column) =
+  match column with
+    (_,[])     -> []
+    | (_,h::t) -> if a = h
+                   then "true"::(generateBoolList(a , ("temp", t)))
+                   else "false"::(generateBoolList(a , ("temp",t)))
+
+(* Projection *)
+let rec projection (attribute, dtable) =
+  match attribute with
+    []   -> []
+  | h::t -> getColumn(dtable, h)::projection(t, dtable)
+
+(* Rename *)
+let rec replace (attOld, attNew, dcolumn) =
+  match dcolumn with
+    []         -> raise DoesNotExist
+    | (a,b)::t -> if a = attOld
+              then (attNew, b)::t
+              else (a,b)::(replace(attOld, attNew, t))
+
+let rename (attOld, attNew, dtable) =
+  match dtable with
+    (name, dcolumn) -> replace(attOld, attNew, dcolumn)
+
+(* Restrict *)
+let rec restrictAttrib (boolList, column) =
+  match (boolList, column) with
+    ([], _)          -> []
+  | (_, [])          -> []
+  | (h1::t1, h2::t2) -> if h1 = "true"
+                        then h2::(restrictAttrib(t1, t2))
+                        else restrictAttrib(t1, t2)
+
+let rec restrictColumns (boolList, columns) =
+  match columns with
+    []         -> []
+    | (a,b)::t -> (a, restrictAttrib(boolList, b))::(restrictColumns(boolList, t))
+
+let restriction (attribute, a, dtable) =
+  let boolList = generateBoolList(a, getColumn(dtable, attribute)) in
+    match dtable with
+      (name, columns) -> restrictColumns(boolList, columns)
+
+(* Engine *)
+let rec lookup (var, env) =
+  match env with
+    []               -> raise DoesNotExist
+  | (tname,table)::t -> if var = tname
+                          then (tname,table)
+                          else lookup (var, t)
+
+let eval (relalg, newtable, database) =
+  match relalg with
+       (Project(attribs, table))            -> let ntable = lookup(table, !database) in (newtable, projection(attribs, ntable))::(!dbase)
+    |  (Rename (oldatrib, newatrib, table)) -> let ntable = lookup(table, !database) in (newtable, rename(oldatrib, newatrib, ntable))::(!dbase)
+    |  (Restrict (atrib, value, table))     -> let ntable = lookup(table, !database) in (newtable, restriction(atrib, value, ntable))::(!dbase)
--- OCaml_alfred.ml
+++ OCaml_alfred.ml
@@ -1,5 +1,4 @@
-exception ColumnDoesNotExist
-exception TableDoesNotExist
+exception DoesNotExist
 type attribute = string
 type dcolumn = string * attribute list
 type dtable = string * dcolumn list
@@ -14,51 +13,14 @@ let table = ("Student", [
    ("Course",["Geography";"Economics";"Informatics";"Geography"])
 ])
 
+(* General *)
 let fst(a,b) = a
+
 let snd(a,b) = b
 
-
-(* Projection *)
-let rec projection (attribute, dtable) =
-  match attribute with
-    []     -> []
-  | h::t -> getColumn(dtable, h)::projection(t, dtable)
-
-(* Rename *)
-let rename (attOld, attNew, dtable) =
-  match dtable with
-    (name, dcolumn) -> (name, replace(attOld, attNew, dcolumn))
-
-let rec replace (attOld, attNew, dcolumn) =
-  match dcolumn with
-    []     -> raise ColumnDoesNotExist
-    | h::t -> if fst(h) = attOld
-              then (attNew, snd(h))::t
-              else h::(replace(attOld, attNew, t))
-
-(* Restrict *)
-let restriction (attribute, a, dtable) =
-  let boolList = generateBoolList(a, getColumn(dtable, attribute)) in
-    match dtable with
-      (name, columns) -> (name, restrictColumns(boolList, columns))
-
-let rec restrictColumns (boolList, columns) =
-  match columns with
-    []     -> []
-    | h::t -> (fst(h), restrictAttrib(boolList, snd(h)))::(restrictColumns(boolList, t))
-
-let rec restrictAttrib (boolList, column) =
-  match (boolList, column) with
-    ([], _)          -> []
-  | (_, [])          -> []
-  | (h1::t1, h2::t2) -> if h1 = "true"
-                        then h2::(restrictAttrib(t1, t2))
-                        else restrictAttrib(t1, t2)
-
-(* General *)
 let rec getColumn (dtable, attribute) =
   match dtable with
-    (name, [])     -> raise ColumnDoesNotExist
+    (name, [])     -> raise DoesNotExist
   | (name, h::t) -> if fst(h) = attribute
                       then h
                       else getColumn((name, t), attribute)
@@ -70,9 +32,46 @@ let rec generateBoolList (a, column) =
                    then "true"::(generateBoolList(a , ("temp", t)))
                    else "false"::(generateBoolList(a , ("temp",t)))
 
+(* Projection *)
+let rec projection (attribute, dtable) =
+  match attribute with
+    []     -> []
+  | h::t -> getColumn(dtable, h)::projection(t, dtable)
+
+(* Rename *)
+let rec replace (attOld, attNew, dcolumn) =
+  match dcolumn with
+    []     -> raise DoesNotExist
+    | h::t -> if fst(h) = attOld
+              then (attNew, snd(h))::t
+              else h::(replace(attOld, attNew, t))
+
+let rename (attOld, attNew, dtable) =
+  match dtable with
+    (name, dcolumn) -> replace(attOld, attNew, dcolumn)
+
+(* Restrict *)
+let rec restrictAttrib (boolList, column) =
+  match (boolList, column) with
+    ([], _)          -> []
+  | (_, [])          -> []
+  | (h1::t1, h2::t2) -> if h1 = "true"
+                        then h2::(restrictAttrib(t1, t2))
+                        else restrictAttrib(t1, t2)
+
+let rec restrictColumns (boolList, columns) =
+  match columns with
+    []     -> []
+    | h::t -> (fst(h), restrictAttrib(boolList, snd(h)))::(restrictColumns(boolList, t))
+
+let restriction (attribute, a, dtable) =
+  let boolList = generateBoolList(a, getColumn(dtable, attribute)) in
+    match dtable with
+      (name, columns) -> restrictColumns(boolList, columns)
+
 (* Engine *)
 let eval (q, newtable, database) =
   match q with
-    (Project (attribs, table))   -> projection(attribs, table)
-    |  (Rename (oldatrib, newatrib, table)) -> rename(oldatrib, newatrib, table)
-    |  (Restrict (atrib, val, table)) -> restriction(atrib,val,table)
+    (Project (attribs, table)) -> (newtable, projection(attribs, database))
+    |  (Rename (oldatrib, newatrib, table)) -> (newtable, rename(oldatrib, newatrib, database))
+    |  (Restrict (atrib, value, table)) -> (newtable, restriction(atrib, value, database))
